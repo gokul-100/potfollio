8c00d1f347210b0126e58d9fa7af3ecc8c844f6d
gokul-100 <gokulakanna1006@gmail.com>
2024-08-21 18:17:39 +0530

    updated

--- main/models.py
+++ main/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_time = models.DateTimeField(auto_now_add=True)
+    card_title = models.CharField(max_length=100)
+    card_description = models.TextField()
+    img_url = models.ImageField(upload_to='images/', blank=True, null=True)
+    event_date = models.DateField(auto_now=True, auto_now_add=False)  # For storing date only
+    event_time = models.TimeField(auto_now=True, auto_now_add=False)  # For storing time only
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+
+    def save(self, *args, **kwargs):
+        # self.created_at = slugify(f'{self.event_date} {self.event_time}')
+        super().save(*args,**kwargs)
 
     def __str__(self):
-        return self.title
+        return self.card_title
 
 # Create your models here.
